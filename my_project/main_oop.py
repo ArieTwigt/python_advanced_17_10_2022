c0de3f6ff4b717de47e71382b28445aab566c325
Arie Twigt <ariekanarie@twigtd4.nl>
2022-10-18 14:30:23 +0200

    added assignment solutions for oop

--- my_project/main_oop.py
+++ my_project/main_oop.py
@@ -1,10 +1,14 @@
+from typing import Tuple
+
 class Car:
 
 
     # methods
-    def __init__(self, brand:str, model:str, availability: str="available"):
+    def __init__(self, brand:str, model:str, color:str, price: int=0, availability: str="available"):
         self.brand = brand
         self.model = model
+        self.color = color
+        self.price = price
         self.availability = availability
 
     # method to change the availability of the car
@@ -18,19 +22,33 @@ class Car:
         print(f"Status: {self.availability}")
         self.availability = 'sold'
         print(f"Status: {self.availability}")
+
     
+    # method for specifying the price of a car
+    def set_price(self, price: float):
+        print(f"Price {self.price}")
+        self.price = price
+        print(f"Price {self.price}")
+
+    
+    # method for changing the price of the car
+    def change_price(self, amount: float) -> None:
+        old_price = self.price
+        self.price += amount
+        print(f"Price changed from {old_price} to {self.price}")
+
 
     # represent the class
     def __repr__(self) -> str:
-        return f"{self.brand}-{self.model}-{self.availability}"
+        return f"{self.brand}-{self.model}-{self.price}-{self.color}-{self.availability}"
 
 
 if __name__ == '__main__':
 
     # create an instance of the 'Car' class
-    my_car = Car('OPEL', 'vectra')
-    my_car_2 = Car('TOYOTA', 'YARIS')
-    my_car_3 = Car('BMW', 'i3')
+    my_car = Car('OPEL', 'vectra', 'red')
+    my_car_2 = Car('TOYOTA', 'YARIS', 'green')
+    my_car_3 = Car('BMW', 'i3', 'blue')
 
     # create a loop for the cars
     cars_list = [my_car, my_car_2, my_car_3]
